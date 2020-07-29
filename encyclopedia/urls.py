39aceee0e9e109cb34744cb27c257ecca0b06a5f
nicksmith105 <30782766+nicksmith105@users.noreply.github.com>
2020-07-29 15:13:52 -0700

    new, existing, and random page functions ACTUALLY completed

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index")
+    path("wiki/<str:entryname>", views.wiki, name="wiki")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django import forms
+import numpy as np
 
 from . import util
 
@@ -6,5 +10,35 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def existing_entry(request, title):
+    return render (request, "encyclopedia/content.html", {
+        "title": title,
+        "content": util.get_entry(title.upper())
+    })
+
+class CreateNewPage(forms.Form):
+    title = forms.CharField()
+    textarea = forms.CharField(widget=forms.Textarea(attrs={ 'cols': 80, 'rows': 10 }))
+
+def new_page(request):
+    if request.method == 'POST':
+        form = CreateNewPage(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleand_data['textarea']
+            util.save_entry(title, content)
+
+    return render(request, "encyclopedia/newpage.html", {
+        "form": CreateNewPage()
+    })
+
+def random_page(request):
+    n = np.random.randint(1,len(util.list_entries()))
+    title = util.list_entries()[n]
+    return render(request, "encyclopedia/content.html", {
+        "title": title,
+        "content": util.get_entry(title)
     })
 
