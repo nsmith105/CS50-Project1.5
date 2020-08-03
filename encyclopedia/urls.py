c670f80a278a522a16b670dba31454e42f579676
nicksmith105 <30782766+nicksmith105@users.noreply.github.com>
2020-08-03 14:55:19 -0700

    new pages added, functions complete, edit page bugs need to be fixed

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = "encyclopedia"
-
 urlpatterns = [
-    path("", views.index, name="index")
-    path("<str:title>", views.view_page, name="view_page")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.view_page, name="entry"),
+    path("search", views.search, name="search"),
+    path("new_page", views.new_page, name="new_page"),
+    path("edit_page/<str:title>", views.edit_page, name="edit_page"),
+    path("random_page", views.random_page, name="random_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from markdown2 import markdown
+from random import choice
 
 from . import util
 
@@ -9,8 +11,70 @@ def index(request):
     })
 
 def view_page(request, title):
-    file_name = util.search_for_file(request, title)
+    content = util.get_entry(title)
+    if not content:
+        return render(request, "encyclopedia/page_not_found.html", {
+            "message": "\"" + title + "\" has no entry. Maybe you should create one? :)"
+        })
+    return render(request, "encyclopedia/view_page.html", {
+        "title": title, 
+        "content": markdown(content)
+    })
 
-    content = util.md_to_html(file_name)
+def search(request):
+    keyword = request.GET.get("q")
+    content = util.get_entry(keyword)
+    if not content:
+        result = []
+        for title in util.list_entries():
+            if keyword.casefold() in title.casefold():
+                result.appent(title)
+        return render(request, "encyclopedia/search.html", {
+            "result": result
+        })
+    return render(request, "encyclopedia/view_page.html", {
+        "title": keyword, 
+        "content": markdown(content)
+    })
 
-    
+def new_page(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        for entry in util.list_entries():
+            if title.casefold() == entry.casefold():
+                return render(request, "encyclopedia/new_page.html", {
+                    "message": "Your entry already exists!",
+                    "title": title,
+                    "content": content
+                })
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "message": "New entry successfully added"
+        })
+    return render(request, "encyclopedia/new_page.html")
+
+def edit_page(request, title):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title, 
+            "content": markdown(content),
+            "message": "\"" + title + "\" has been successfully updated"
+        })
+    content = util.get_entry(title)
+    return render(request, "encyclopedia/edit_page.html", {
+        "title": title, 
+        "content": content
+    })
+
+def random_page(request):
+    title = choice(util.list_entries())
+    content = util.get_entry(title)
+    return render(request, "encyclopedia/view_page.html", {
+        "title": title, 
+        "content": markdown(content)
+    })
